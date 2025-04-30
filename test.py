6a90bfc758bfb3d321f85f0d23cfbca07dc59a2f
kingplutoo010 <163101341+kingplutoo010@users.noreply.github.com>
2025-04-30 15:19:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,42 @@
 print ("Wassup Buddy!! v7711-baba")
 
+pipeline {
+    agent any
+
+    environment {
+        MAVEN_VERSION = '3.9.6'
+    }
+
+        stage('Install Maven') {
+            steps {
+                sh '''
+                if ! [ -x "$(command -v mvn)" ]; then
+                    echo "Installing Maven..."
+                    wget https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+                    tar xzf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+                    sudo mv apache-maven-${MAVEN_VERSION} /opt/maven
+                    export PATH=/opt/maven/bin:$PATH
+                    echo "export PATH=/opt/maven/bin:$PATH" >> ~/.bashrc
+                else
+                    echo "Maven is already installed."
+                fi
+                mvn -version
+                '''
+            }
+        }
+
+        stage('Run Maven Build') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'Pipeline Finished!'
+        }
+    }
+}
+
+
